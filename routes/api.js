2e902247dcc0cbdd138a73738a22494bd887c98a
Jay Jenks <jamesljenks@gmail.com>
2021-08-05 21:24:42 -0400

    Added htmlRoutes.js to routes folder and updated api.js in routes folder

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,62 @@
 const router = require("express").Router();
-const Exercise = require("../models/exercise.js");
+const { response } = require("express");
+const Workout = require("../models/workout.js");
 
-router.post("/api/exercise", ({ body }, res) => {
-  Exercise.create(body)
-    .then(dbExercise => {
-      res.json(dbTransaction);
+router.post("/api/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.post("/api/exercise/bulk", ({ body }, res) => {
-  Exercise.insertMany(body)
-    .then(dbExercise => {
-      res.json(dbExercise);
+router.post("/api/workout/bulk", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
     });
 });
 
-router.get("/api/exercise", (req, res) => {
-  Exercise.find({})
+router.get("/api/workout", (req, res) => {
+  Workout.find({})
     .sort({ date: -1 })
-    .then(dbExercise => {
-      res.json(dbExercise);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workout:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workout", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((err) => {
       res.status(400).json(err);
     });
 });
